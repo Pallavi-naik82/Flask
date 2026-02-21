a5492f03702a00d012642e183278fa22d3914988
Pallavi <pallavinaik8296@gmail.com>
2026-02-21 09:58:37 +0530

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,27 @@
+import requests
+
+apikey = "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"
+
+word = input("enter your word: ")
+
+res = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}")
+
+
+print("status_code:",res.status_code)
+
+if res.status_code == 200:
+    meanings= res.json()[0]["meanings"]
+    for meaning in meanings:
+        defn = meaning['definitions']
+        for d in defn:
+            print(d['definition'])  
+else:
+    print("Word not found in dictionary")         
+    
+
+    
+
+    
+
+
+
